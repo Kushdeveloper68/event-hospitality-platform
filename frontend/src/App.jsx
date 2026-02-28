93796d38524abd8e5fca5f88033ffac45deb63fc
kushdeveloper68 <kushpandit68775@gmail.com>
2026-02-28 17:23:51 +0530

    dashboard routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
 import {BrowserRouter , Routes, Route} from "react-router-dom"
-import {UserSignup, UserLogin, PageNotFound} from "./pages"
-
+// dashboard pages
+import {PlatformLandingPage,EventDirectory, MainOprationDashboard} from "./pages"
+// form pages
+import {UserSignup, UserLogin} from "./pages"
+// others pages 
+import {PageNotFound} from "./pages"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<PlatformLandingPage />} />
+        <Route path="/dashboard" element={<MainOprationDashboard />} />
         <Route path="/signup" element={<UserSignup />} />
         <Route path="/login" element={<UserLogin />} />
         <Route path="*" element={<PageNotFound />} />
