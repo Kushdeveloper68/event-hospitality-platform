64ff96065934fac45226f75c14b4e04599484102
kushdeveloper68 <kushpandit68775@gmail.com>
2026-03-01 11:07:14 +0530

    clear routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,46 @@
-import './App.css'
-import {BrowserRouter , Routes, Route} from "react-router-dom"
-import DashboardLayout from './layouts/DashboardLayout'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DashboardLayout from "./layouts/DashboardLayout";
 // dashboard pages
-import {PlatformLandingPage, EventDirectory, MainOprationDashboard, EventAnalyticsReports, OrganizationAnalyticsDashboards} from "./pages"
+import {
+  PlatformLandingPage,
+  EventDirectory,
+  MainOprationDashboard,
+  EventAnalyticsReports,
+  OrganizationAnalyticsDashboards,
+} from "./pages";
 // form pages
-import {UserSignup, UserLogin} from "./pages"
+import { UserSignup, UserLogin, CreateNewEvent } from "./pages";
 // settings / others
-import {OragnizationSetting, PageNotFound} from "./pages"
+import { OragnizationSetting, PageNotFound } from "./pages";
 
 function App() {
   return (
     <BrowserRouter>
+      <DashboardLayout>
+        <Routes>
+          {/* Dashboard routes wrapped with layout */}
+          <Route path="/dashboard" element={<MainOprationDashboard />} />
+          <Route path="/events" element={<EventDirectory />} />
+          <Route path="/reports" element={<EventAnalyticsReports />} />
+          <Route
+            path="/analytics"
+            element={<OrganizationAnalyticsDashboards />}
+          />
+          <Route path="/settings" element={<OragnizationSetting />} />
+          <Route path="/create-event" element={<CreateNewEvent />} />
+        </Routes>
+      </DashboardLayout>
+
       <Routes>
+        {/* Public routes */}
         <Route path="/" element={<PlatformLandingPage />} />
         <Route path="/signup" element={<UserSignup />} />
         <Route path="/login" element={<UserLogin />} />
-
-        {/* Dashboard routes wrapped with layout */}
-        <Route path="/dashboard" element={<DashboardLayout><MainOprationDashboard /></DashboardLayout>} />
-        <Route path="/events" element={<DashboardLayout><EventDirectory /></DashboardLayout>} />
-        <Route path="/reports" element={<DashboardLayout><EventAnalyticsReports /></DashboardLayout>} />
-        <Route path="/analytics" element={<DashboardLayout><OrganizationAnalyticsDashboards /></DashboardLayout>} />
-        <Route path="/settings" element={<DashboardLayout><OragnizationSetting /></DashboardLayout>} />
-
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
