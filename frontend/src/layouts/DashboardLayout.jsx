ff0b1285dd5f5efba1fcbb7a103b99d65b8d2402
Developer Kush 👨🏼‍💻 || Full stack web developer 💪🏼 <kushpandit68775@gmail.com>
2026-05-31 09:47:43 +0530

    adding global footer

--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import DashboardNavbar from '../components/DashboardNavbar'
-
+import {Footer} from '../components/'
 function DashboardLayout({ children }) {
 	return (
 		<div className="flex h-screen overflow-hidden">
@@ -8,6 +8,7 @@ function DashboardLayout({ children }) {
 			<div className="flex-1 flex flex-col min-w-0 overflow-hidden">
 				<main className="flex-1 overflow-y-auto custom-scrollbar dark:bg-slate-950">
 					{children}
+					<Footer />
 				</main>
 			</div>
 		</div>
