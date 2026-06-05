7a1567cb5d3573a7f22791a6fadd91b4d7d44244
Developer Kush 👨🏼‍💻 || Full stack web developer 💪🏼 <kushpandit68775@gmail.com>
2026-06-05 17:29:46 +0530

    Update mongoose version to 9.6.3 and fix route case sensitivity in server.js

--- backend/connections/mongodbConnection.js
+++ backend/connections/mongodbConnection.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectToMongoDB(url) {
     try {
+       
         await mongoose.connect(url);
         console.log('Connected to MongoDB');
     } catch (error) {
