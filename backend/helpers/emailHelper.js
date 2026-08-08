a8567c6f3d4c9834531faf030e9eb8c84374062e
Developer Kush 👨🏼‍💻 || Full stack web developer 💪🏼 <kushpandit68775@gmail.com>
2026-08-08 16:05:21 +0530

    Refactor email transport configuration to use SMTP settings for improved reliability

--- backend/helpers/emailHelper.js
+++ backend/helpers/emailHelper.js
@@ -6,7 +6,10 @@ if (!process.env.EMAIL_USER || !process.env.EMAIL_PASSWORD) {
 
 // Create reusable transporter
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  family: 4,
     auth: {
         user: process.env.EMAIL_USER ,
         pass: process.env.EMAIL_PASSWORD 
