8c06de031a1d1d5b263b2a05ae4cf7cf2810051c
Developer Kush 👨🏼‍💻 || Full stack web developer 💪🏼 <kushpandit68775@gmail.com>
2026-08-19 20:38:57 +0530

    checking private-public repo deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+ 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
@@ -8,7 +8,7 @@ const api = axios.create({
   },
 });
 
-/**
+/** 
  * Request Interceptor
  */
 api.interceptors.request.use(
